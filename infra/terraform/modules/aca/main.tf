916ad05c2626cfedcc5cfaef8540f83c46f147d2
vietanhdo <vietanhdo.6898@gmail.com>
2026-06-12 23:32:35 +0700

    fix: update frontend target port to 8080

--- infra/terraform/modules/aca/main.tf
+++ infra/terraform/modules/aca/main.tf
@@ -116,7 +116,7 @@ resource "azurerm_container_app" "frontend" {
   ingress {
     allow_insecure_connections = false
     external_enabled           = true
-    target_port                = 80
+    target_port                = 8080
     transport                  = "auto"
 
     traffic_weight {
