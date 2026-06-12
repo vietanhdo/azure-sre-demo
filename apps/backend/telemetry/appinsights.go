7ab86f7bfeeb63f32058571ef42005c94608d49b
vietanhdo <vietanhdo.6898@gmail.com>
2026-06-13 00:50:23 +0700

    fix: AppInsights instrumentation key parsing

--- apps/backend/telemetry/appinsights.go
+++ apps/backend/telemetry/appinsights.go
@@ -2,6 +2,7 @@ package telemetry
 
 import (
 	"os"
+	"strings"
 
 	"github.com/microsoft/ApplicationInsights-Go/appinsights"
 )
@@ -10,8 +11,24 @@ var AppInsightsClient appinsights.TelemetryClient
 
 func InitAppInsights() func() {
 	connStr := os.Getenv("APPLICATIONINSIGHTS_CONNECTION_STRING")
-	if connStr != "" {
-		AppInsightsClient = appinsights.NewTelemetryClient(connStr)
+	iKey := os.Getenv("APPINSIGHTS_INSTRUMENTATIONKEY")
+	
+	if iKey == "" && connStr != "" {
+		// Parse Connection String to extract Instrumentation Key
+		parts := strings.Split(connStr, ";")
+		for _, part := range parts {
+			if strings.HasPrefix(part, "InstrumentationKey=") {
+				iKey = strings.TrimPrefix(part, "InstrumentationKey=")
+				break
+			}
+		}
+	}
+
+	if iKey != "" {
+		AppInsightsClient = appinsights.NewTelemetryClient(iKey)
+		// Set Cloud Role Name for Application Map
+		AppInsightsClient.Context().Tags.Cloud().SetRole("backend")
+		
 		// Return a func to flush on shutdown
 		return func() {
 			select {
